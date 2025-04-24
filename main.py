2bd4e6f0b7c315d53e9c45ffa4d96d558cb04960
Anfisa <anfis.igorevna@gmail.com>
2025-04-24 19:05:35 +0300

    Дата и время

---
+++ main.py
@@ -0,0 +1,72 @@
+# Импортируем необходимые классы.
+import datetime
+
+from telegram.ext import Application, MessageHandler, filters, CommandHandler
+
+from config import BOT_TOKEN
+
+
+async def start(update, context):
+    """Отправляет сообщение когда получена команда /start"""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
+    )
+
+
+async def help_command(update, context):
+    """Отправляет сообщение когда получена команда /help"""
+    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+
+
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
+async def echo(update, context):
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
+    await update.message.reply_text(f"Я получил сообщение {update.message.text}.")
+
+
+async def time(update, context):
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
+    await update.message.reply_text(f"Сейчас {datetime.datetime.now().strftime('%H:%M:%S')}")
+
+
+async def date(update, context):
+    await update.message.reply_text(f"Сегодня {datetime.datetime.today().strftime('%d.%m.%Y')}")
+
+
+def main():
+    # Создаём объект Application.
+    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    # Создаём обработчик сообщений типа filters.TEXT
+    # из описанной выше асинхронной функции echo()
+    # После регистрации обработчика в приложении
+    # эта асинхронная функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("time", time))
+    application.add_handler(CommandHandler("date", date))
+
+    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
+
+    # Регистрируем обработчик в приложении.
+    application.add_handler(text_handler)
+
+    # Запускаем приложение.
+    application.run_polling()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()
