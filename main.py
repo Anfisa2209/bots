9b746adde32bc37ff407239b087398b28d937a5a
Anfisa <anfis.igorevna@gmail.com>
2025-05-15 11:24:07 +0300

    Бот-экскурсовод или «в греческом зале...»

--- main.py
+++ main.py
@@ -1,58 +1,72 @@
-from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler
 
 from config import BOT_TOKEN
 
+hols = {
+    1: [
+        [InlineKeyboardButton('Зал 2 - скульптуры', callback_data='go_2')],
+        [InlineKeyboardButton('Выход', callback_data='button_exit')]
+    ],
+    2: [[InlineKeyboardButton('Зал 3 - картины', callback_data='go_3')]],
+    3: [
+        [InlineKeyboardButton('Зал 1 - хол', callback_data='go_1')],
+        [InlineKeyboardButton('Зал 4 - буфет', callback_data='go_4')]
+    ],
+    4: [[InlineKeyboardButton('Зал 1 - хол', callback_data='go_1')]]
+}
+
 
 async def start(update, context):
+    reply_markup = InlineKeyboardMarkup(hols[1])
     await update.message.reply_text(
-        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
-        "Вы можете прервать опрос, послав команду /stop.\n"
-        "В каком городе вы живёте? Если не хотите отвечать, пошлите команду /skip")
-
-    return 1
-
-
-async def skip(update, context):
-    await update.message.reply_text('Какая погода у вас за окном?')
-    return 2
+        'Добро пожаловать! Пожалуйста, сдайте верхнюю одежду в гардероб!\n'
+        'Вы в холе (зал 1).\n'
+        'Куда пойти дальше? Выбирайте:',
+        reply_markup=reply_markup
+    )
 
 
-async def first_response(update, context):
-    locality = update.message.text
-    await update.message.reply_text(
-        f"Какая погода в городе {locality}?")
-    return 2
+async def button_callback(update, context):
+    query = update.callback_query
+    await query.answer()
 
-
-async def second_response(update, context):
-    # Ответ на второй вопрос.
-    # Мы можем его сохранить в базе данных или переслать куда-либо.
-    weather = update.message.text
-    await update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
-    return ConversationHandler.END  # Константа, означающая конец диалога.
-    # Все обработчики из states и fallbacks становятся неактивными.
-
-
-async def stop(update, context):
-    await update.message.reply_text("Всего доброго!")
-    return ConversationHandler.END
+    if query.data == 'go_2':
+        await query.message.reply_text(
+            'Добро пожаловать! Пожалуйста, сдайте верхнюю одежду в гардероб!\n'
+            'В этом зале вы видите скульптуры (зал 2).\n'
+            'Куда пойти дальше? Выбирайте:',
+            reply_markup=InlineKeyboardMarkup(hols[2])
+        )
+    elif query.data == 'button_exit':
+        await query.message.reply_text('Всего доброго, не забудьте забрать верхнюю одежду в гардеробе!')
+    elif query.data == 'go_1':
+        await query.message.reply_text(
+            'Добро пожаловать! Пожалуйста, сдайте верхнюю одежду в гардероб!\n'
+            'Вы в холе (зал 1).\n'
+            'Куда пойти дальше? Выбирайте:',
+            reply_markup=InlineKeyboardMarkup(hols[1])
+        )
+    elif query.data == 'go_3':
+        await query.message.reply_text(
+            'Добро пожаловать! Пожалуйста, сдайте верхнюю одежду в гардероб!\n'
+            'В этом зале вы видите картины (зал 3).\n'
+            'Куда пойти дальше? Выбирайте:',
+            reply_markup=InlineKeyboardMarkup(hols[3])
+        )
+    elif query.data == 'go_4':
+        await query.message.reply_text(
+            'Добро пожаловать! Пожалуйста, сдайте верхнюю одежду в гардероб!\n'
+            'В этом зале можно поесть (зал 4).\n'
+            'Куда пойти дальше? Выбирайте:',
+            reply_markup=InlineKeyboardMarkup(hols[4])
+        )
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
-
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            # Функция читает ответ на первый вопрос и задаёт второй.
-            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
-            # Функция читает ответ на второй вопрос и завершает диалог.
-            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)]
-        },
-        fallbacks=[CommandHandler('stop', stop), CommandHandler('skip', skip)]
-    )
-
-    application.add_handler(conv_handler)
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(button_callback))
     application.run_polling()
 
 
