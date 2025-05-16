e6ffb337a8eaebd60d18428c9ee7e9328c08d17e
Anfisa <anfis.igorevna@gmail.com>
2025-05-16 14:52:29 +0300

    Бот «Тестирующая система»

--- main.py
+++ main.py
@@ -1,52 +1,99 @@
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+import json
+import random
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 
 from config import BOT_TOKEN
 
-poet = [i.strip() for i in open('poet.txt', encoding='utf8').readlines() if i.strip()]
+with open('questions.json', encoding='utf8') as json_file:
+    data = json.load(json_file)['questions']
 
 
 async def start(update, context):
-    context.user_data['cur_line'] = 0
-    await update.message.reply_text(poet[0])
+    context.user_data['answers'] = {'questions': [], 'right_answers': 0, 'wrong_answers': 0}
+    context.user_data['game_on'] = False
+
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Да', callback_data='yes')],
+                                         [InlineKeyboardButton('Нет', callback_data='no')]])
+
+    await update.message.reply_text(
+        'Знание литературы проверили, теперь проверим знание истории😊. Ответьте на 10 вопросов.',
+        reply_markup=reply_markup)
 
 
-async def check_line(update, context):
+async def ask_question(context, chat_id):
     user_data = context.user_data
-    cur_line = user_data.get('cur_line', 0)
+    answers = user_data['answers']
 
-    user_text = update.message.text
-    correct_text = poet[cur_line + 1] if cur_line + 1 < len(poet) else ""
+    # Выбираем вопрос, которого ещё не было
+    available_questions = [q for q in data if q['question'] not in [item[0] for item in answers['questions']]]
 
-    if user_text == correct_text:
-        user_data['cur_line'] = cur_line + 2
-        if user_data['cur_line'] >= len(poet):
-            await update.message.reply_text("Радость-то какая! Мы смогли! Давайте еще раз прочитаем?\nЕсли хотите повторить, нажмите /start")
-        else:
-            await update.message.reply_text(poet[user_data['cur_line']])
-    else:
-        await update.message.reply_text('Нет, не так')
-        await suphler(update, context)
+    if not available_questions:
+        await context.bot.send_message(chat_id, "Все вопросы закончились!")
+        return
+    question_data = random.choice(available_questions)
+    question = question_data['question']
+    response = question_data['response']
 
+    answers['questions'].append((question, response))
 
-async def suphler(update, context):
-    user_data = context.user_data
-    cur_line = user_data.get('cur_line', 0)
-    if cur_line + 1 < len(poet):
-        right_line = poet[cur_line + 1]
-        await update.message.reply_text(f'Подсказка:\n{right_line[:len(right_line) // 2]}...')
+    await context.bot.send_message(
+        chat_id,
+        f'Уважаемые знатоки, внимание на экран:\nВопрос {len(answers["questions"])}: {question}'
+    )
 
 
 async def stop(update, context):
     context.user_data.clear()
-    await update.message.reply_text('Ужас, уходите, не дочитав стих! Ну и ладно, до свидания!')
+    await update.message.reply_text('Как, уже уходите? Ну и ладно, до свидания!')
+
+
+async def button_callback(update, context):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == 'yes':
+        await query.edit_message_text(text='Да начнется игра!')
+        context.user_data['game_on'] = True
+        chat_id = query.message.chat.id
+        await ask_question(context, chat_id)
+    elif query.data == 'no':
+        await query.edit_message_text(text='Не хотите проверять знания по истории? Ну и ладно! До свидания!')
+
+
+async def check_response(update, context):
+    if context.user_data.get('game_on', False):
+        user_answer = update.message.text
+        answers = context.user_data['answers']
+
+        current_question = answers['questions'][-1]
+        right_answer = current_question[1]
+
+        if user_answer.strip() == right_answer.strip():
+            answers['right_answers'] += 1
+            await update.message.reply_text('Совершенно верно!')
+        else:
+            answers['wrong_answers'] += 1
+            await update.message.reply_text(f'Неверно! Правильный ответ: {right_answer}. Пойдемте дальше')
+
+        if len(answers['questions']) >= 10:
+            await update.message.reply_text(
+                f"Игра окончена! Правильных ответов: {answers['right_answers']}, "
+                f"Неправильных: {answers['wrong_answers']}"
+            )
+            context.user_data.clear()
+        else:
+            chat_id = update.message.chat.id
+            await ask_question(context, chat_id)
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("stop", stop))
-
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_line))
+    application.add_handler(CallbackQueryHandler(button_callback))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_response))
     application.run_polling()
 
 
